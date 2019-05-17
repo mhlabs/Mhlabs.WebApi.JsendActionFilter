d3036434af40e8fb00a336bea6b7e1785abac3ef
Jens-Peter Olsson <jens-peter.olsson@it-budet.se>
2019-05-17 11:44:20 +0200

    chore: adding test project for build to succeed and upping version since new error handling might be a breaking change for some clients

---
+++ Mhlabs.WebApi.JsendActionFilter.Tests/DefaultTest.cs
@@ -0,0 +1,13 @@
+using Xunit;
+
+namespace Mhlabs.WebApi.JsendActionFilter.Tests
+{
+    public class DefaultTest
+    {
+        [Fact]
+        public void This_Is_True()
+        {
+            Assert.True(true);
+        }
+    }
+}
